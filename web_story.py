bd7f9d45df612f3db97f50cdabe04cbdc4db31bc
Paul <NguyenGPhuc@gmail.com>
2023-11-14 20:49:16 -0800

    able to display translation (WIP) formatting

--- web_story.py
+++ web_story.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import openai_service
 
 app = Flask(__name__)
@@ -10,23 +10,25 @@ def home():
 
 
 @app.route('/translateText', methods=['POST'])
-def picture_prompt():
+def translated_text():
     if request.method == "POST":
 
+        inputText = request.form.get('inputText')
         translatedText = ''
 
         #Funtion for translating from vietnamese to english
         try:
-            inputText = request.form.get('inputText')
             translatedText = openai_service.translate_viet2en(request.form.get('inputText'))
 
         except:
             print('Unable to call from API')
+        
+        return jsonify({'translatedText': translatedText, 'inputText': inputText})
 
     else: 
-        print ("Input was not correctly process")
+        print ("User input was not processed correctly")
 
-    return render_template('index.html', inputText=inputText, translatedText=translatedText)
+    return render_template('index.html', inputText='', translatedText='')
 
 if __name__ == '__main__':
     app.run(debug = True)
