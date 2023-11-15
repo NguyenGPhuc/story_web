d37921ded82ed4d2804e9dcf812a3e5f6798889d
Paul <NguyenGPhuc@gmail.com>
2023-11-14 21:49:22 -0800

    correctly formated translated text

--- web_story.py
+++ web_story.py
@@ -13,20 +13,26 @@ def home():
 def translated_text():
     if request.method == "POST":
 
+        # Get user input (Vietamese text)
         inputText = request.form.get('inputText')
         translatedText = ''
 
         #Funtion for translating from vietnamese to english
         try:
+            # Gets transltion text from API
             translatedText = openai_service.translate_viet2en(request.form.get('inputText'))
+            # Remove any extra characters winthin the translation string
+            cleanTranslate = translatedText.replace('\n', '').replace('"', '').replace('.', '')
 
         except:
             print('Unable to call from API')
         
-        return jsonify({'translatedText': translatedText, 'inputText': inputText})
+        # Return translated text in json format
+        return jsonify({'translatedText': cleanTranslate, 'inputText': inputText})
 
     else: 
         print ("User input was not processed correctly")
+
 
     return render_template('index.html', inputText='', translatedText='')
 
