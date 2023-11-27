be50731ef0e6250123b2ec3eb58bf2b59c3ecb9a
Paul <NguyenGPhuc@gmail.com>
2023-11-26 22:50:26 -0800

    functional dropdown menu

--- README.md
+++ README.md
@@ -14,12 +14,12 @@ Download and install Python - https://www.python.org/downloads/
 Flask - A micro web framework written in Python. It is classified as a microframework because it does not require particular tools or libraries. It has no database abstraction layer, form validation, or any other components where pre-existing third-party libraries provide common functions - Wikipedia
 
 OpenAI (GPT-3.5) - An artificial intelligence research laboratory consisting of the for-profit subsidiary OpenAI LP and its parent company, the non-profit OpenAI Inc. 
-- pip install openai
 <Your own OpenAI API key>
 Sign up here -> https://beta.openai.com/signup
 
+1. Create a new file within this directory call 'config.py'
+2. Write the API key in this format : openai_key = 'sk-abc123...'
+
+
 *** Instruction ***
 1. run 'start_here.bat' - Check for virual environment and install venv and all dependencies if not found. Will start local server automatically when finish.
-2. Descibe the image as detail as you in any language in "Input Text" square. OR you can type it in "Translated Text (English)" directly. Select the desire dimension  then click "Generate" button.
-3. QUICK GENERATE* Type in "Input Text", select dimension and click "Translate & Generate". Option 2 is if the translation to English is off and you want to change it before generation.
-4. All image are saves with "output" folder of the same directory. It will creates one if it doesn't already has it.
--- web_story.py
+++ web_story.py
@@ -12,77 +12,81 @@ app = Flask(__name__)
 # Start default site
 @app.route('/')
 def home():
-   
+
+    # read the options from file
     with open('author.txt', 'r') as file:
-        authors = json.load(file)
+        authors_data = json.load(file)
+        # For missing parameters
+        authors = authors_data.get('authors', [])
     with open ('category.txt', 'r') as file:
-        categories = json.load(file)
+        categories_data = json.load(file)
+        categories = categories_data.get('categories', [])
 
-    return render_template('index.html', authors=authors, categories=categories)
-  
-
-@app.route('/changeTheme', methods=['POST'])
-def change_theme():
-    if request.method == "POST":
-
-        global parseText
-
-        # Get user input (Any language text)
-        inputText = request.form.get('inputText')
-
-        # 
-        try:
-            # Get modified text from API
-            modText = openai_service.re_theme(inputText)
-            # Remove any extra characters winthin the string
-            parseText = modText.replace('\n', '').replace('"', '').replace('.', '')
+    # print(json.dumps(authors))
+    return render_template('index.html', categories=categories, authors=authors)
 
 
-        except:
-            print('Unable to call from API')
+# @app.route('/changeTheme', methods=['POST'])
+# def change_theme():
+#     if request.method == "POST":
+
+#         # Get user input (Any language text)
+#         inputText = request.form.get('inputText')
+
+#         # 
+#         try:
+#             # Get modified text from API
+#             modText = openai_service.re_theme(inputText)
+#             # Remove any extra characters winthin the string
+#             parseText = modText.replace('"', '')
+
+
+#         except Exception as e:
+#             print('Error:', str(e))
+#             return jsonify({'error': 'Unable to call from API'})
         
-        # Return translated text in json format
-        return jsonify({'modText': parseText, 'inputText': inputText})
+#         # Return translated text in json format
+#         return jsonify({'parseText': parseText})
 
-    else: 
-        print ("User input was not processed correctly")
+#     else: 
+#         print ("User input was not processed correctly")
 
 
-    return render_template('index.html', inputText='', modText='')
+#     return render_template('index.html', inputText='', modText='')
 
 # Generate image using translated text
-@app.route('/generateImage', methods=['POST'])
-def generate_image():
-    if request.method == 'POST':
-        try:
+# @app.route('/generateImage', methods=['POST'])
+# def generate_image():
+#     if request.method == 'POST':
+#         try:
 
-            # Set model and image size
-            imageSize = request.form.get('imageDimensions')
-            if imageSize == "512x512":
-                imageModel = 'dall-e-2'
-            else:
-                imageModel = 'dall-e-3'
+#             # Set model and image size
+#             imageSize = request.form.get('imageDimensions')
+#             if imageSize == "512x512":
+#                 imageModel = 'dall-e-2'
+#             else:
+#                 imageModel = 'dall-e-3'
 
 
-            parseText = request.form.get('modTextArea')
+#             parseText = request.form.get('modTextArea')
 
-            # Get image url form API
-            imageUrl = openai_service.prompt_image(imageModel, parseText, imageSize)
+#             # Get image url form API
+#             imageUrl = openai_service.prompt_image(imageModel, parseText, imageSize)
             
 
 
-            if imageUrl is not None:
-                save_image(imageUrl)
+#             if imageUrl is not None:
+#                 save_image(imageUrl)
            
 
-        except Exception as e:
-            logging.exception('Failed to generate image: %s', str(e))
-            return jsonify({'error': 'Failed to generate image'})
+#         except Exception as e:
+#             logging.exception('Failed to generate image: %s', str(e))
+#             return jsonify({'error': 'Failed to generate image'})
 
-        return jsonify({'imageUrl':imageUrl})
-    else:
-        print('Unable to generate image')
-        return jsonify({'error': 'Unable to generate image'})
+#         return jsonify({'imageUrl':imageUrl})
+#     else:
+#         print('Unable to generate image')
+#         return jsonify({'error': 'Unable to generate image'})
 
 
 # # Doesn't display translated text.
@@ -126,26 +130,26 @@ def generate_image():
 #         return jsonify({'error': 'Unable to generate image'})
 
 
-def save_image(imageUrl):
-    try:
-        output_folder = 'output'
-        os.makedirs(output_folder, exist_ok=True)
+# def save_image(imageUrl):
+#     try:
+#         output_folder = 'output'
+#         os.makedirs(output_folder, exist_ok=True)
 
-        # Use date and time for naming
-        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-        imageFilename = f'{timestamp}.jpeg'
-        imagePath = os.path.join(output_folder, imageFilename)
+#         # Use date and time for naming
+#         timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+#         imageFilename = f'{timestamp}.jpeg'
+#         imagePath = os.path.join(output_folder, imageFilename)
 
-        # Download the image from url
-        imageData = requests.get(imageUrl).content
+#         # Download the image from url
+#         imageData = requests.get(imageUrl).content
     
-        # Save image
-        with open(imagePath, 'wb') as f:
-            f.write(imageData)
+#         # Save image
+#         with open(imagePath, 'wb') as f:
+#             f.write(imageData)
 
-    except Exception as e:
-        logging.exception('Failed to save image: %s', str(e))
-        return jsonify({'error': 'Failed to save image'})
+#     except Exception as e:
+#         logging.exception('Failed to save image: %s', str(e))
+#         return jsonify({'error': 'Failed to save image'})
 
 
 if __name__ == '__main__':
